6e412323d7f31c4b121a7a9e5c4c3d6ea8029610
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-09-09 14:24:37 +0200

    added insertCharacter and getUserInput method

--- Hangman.java
+++ Hangman.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Hangman {
 
     public static void main(String[] args) {
         System.out.println("|^^^^^^^^^^^^^^^^^|");
         System.out.println("| Hangman is cool |");
         System.out.println("|_________________|");  
-        choosenWord();
+        String word = choosenWord();
+        char letter = getUserInput();
+        insertCharacter(word,letter);
         
     }
     public static String choosenWord() {
@@ -13,7 +17,20 @@ public class Hangman {
         String word = words[x];
         int wordLength = word.length();
         System.out.println(word);
-        System.out.println(" _".repeat(wordLength));
-        return word;
+        String unknownWord = "_".repeat(wordLength);
+        System.out.println(unknownWord);
+        return unknownWord;
+    }
+    public static void insertCharacter(String word, char letter) {
+        StringBuilder myName = new StringBuilder(word);
+        myName.setCharAt(4, letter);
+        System.out.println(myName);
+    }
+    public static char getUserInput() {
+        System.out.println("Guess a letter: ");
+        Scanner scan = new Scanner(System.in);
+        String guess = scan.next();
+        char letter = guess.charAt(0);
+        return letter;
     }
 }
