cdd951af827ef3b03b92a1a65b22a2694820bc60
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-09-09 13:53:22 +0200

    added choosenWord method

--- Hangman.java
+++ Hangman.java
@@ -1,9 +1,19 @@
 public class Hangman {
 
     public static void main(String[] args) {
-
-        System.out.println("Hangman is cool");
-
+        System.out.println("|^^^^^^^^^^^^^^^^^|");
+        System.out.println("| Hangman is cool |");
+        System.out.println("|_________________|");  
+        choosenWord();
+        
     }
-
+    public static String choosenWord() {
+        String[] words = {"cabbage","moustache","codecool"};
+        int x = (int )(Math.random()* words.length);
+        String word = words[x];
+        int wordLength = word.length();
+        System.out.println(word);
+        System.out.println(" _".repeat(wordLength));
+        return word;
+    }
 }
